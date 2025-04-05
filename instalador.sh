626967d956415c336dc6a09d026d8eee9d9b2a8c
Marcones <141690889+Marconesds1@users.noreply.github.com>
2025-04-05 00:45:58 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 clear
 echo "🔧 Instalando o Painel MARCONES_MS..."
+
+# Atualizando sistema e instalando dependências
 apt-get update -y > /dev/null
 apt-get install -y curl net-tools vnstat speedtest-cli unzip > /dev/null 2>&1
+
+# Baixando o script do GitHub
 cd /usr/local/bin || exit
-wget -O marcones_ms https://raw.githubusercontent.com/MARCONES-MS/scripts/main/script.sh > /dev/null 2>&1
+wget -O marcones_ms https://raw.githubusercontent.com/Marconesds1/script_marcones_ms/main/script.sh > /dev/null 2>&1
 chmod +x marcones_ms
+
+# Rodando o painel
 clear
 marcones_ms
